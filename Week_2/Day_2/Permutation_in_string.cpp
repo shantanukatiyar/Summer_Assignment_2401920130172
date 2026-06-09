dc93772cc66c1aeb97c7122446b834acd8008628
Shantanu Katiyar <shantanukatiyar29@gmail.com>
2026-06-09 18:54:19 +0530

    Create Permutation_in_string.cpp
    Added code

---
+++ Week_2/Day_2/Permutation_in_string.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        
+        int n = s1.size();
+        int m = s2.size();
+
+        if(n > m) return false;
+
+        vector<int> freq1(26, 0);
+        vector<int> freq2(26, 0);
+
+        for(char ch : s1) {
+            freq1[ch - 'a']++;
+        }
+
+        for(int i = 0; i < n; i++) {
+            freq2[s2[i] - 'a']++;
+        }
+
+        if(freq1 == freq2) return true;
+
+        for(int i = n; i < m; i++) {
+
+            freq2[s2[i] - 'a']++;
+
+            freq2[s2[i - n] - 'a']--;
+
+            if(freq1 == freq2)
+                return true;
+        }
+
+        return false;
+    }
+};
