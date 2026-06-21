80c9c3187d9a49c1089f36eaa73d317671d775df
Shantanu Katiyar <shantanukatiyar29@gmail.com>
2026-06-21 13:06:01 +0530

    Create Next_greater_element_I.cpp
    Added Code

---
+++ Week_3/Day_3/Next_greater_element_I.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> nextGreater;
+        stack<int> st;
+
+        for (int num : nums2) {
+            while (!st.empty() && num > st.top()) {
+                nextGreater[st.top()] = num;
+                st.pop();
+            }
+            st.push(num);
+        }
+
+        vector<int> ans;
+
+        for (int num : nums1) {
+            if (nextGreater.count(num))
+                ans.push_back(nextGreater[num]);
+            else
+                ans.push_back(-1);
+        }
+
+        return ans;
+    }
+};
